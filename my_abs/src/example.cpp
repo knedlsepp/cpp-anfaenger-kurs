a39fcd610cf522251b76810d08a1b1dfbcb8d015
Josef Kemetmüller <josef.kemetmueller@aon.at>
2015-09-30 22:00:30 +0200

    Finalize abs

---
+++ my_abs/src/example.cpp
@@ -0,0 +1,4 @@
+#include "example.hpp"
+auto my_abs(double x) -> double{
+    return (x<0) ? -x : x;
+}
--- my_abs/tests/test_all.cpp
+++ my_abs/tests/test_all.cpp
@@ -1,27 +1,28 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include "example.hpp"
 #include <vector>
 
-TEST_CASE( "When computing absolute values", "[abs]" ) {
+TEST_CASE( "When computing my_absolute values", "[my_abs]" ) {
 
     SECTION( "Negative numbers become positive" ) {
-        REQUIRE( abs(-3) == 3 );
-        REQUIRE( abs(-1e3) == 1e3 );
-        REQUIRE( abs(-4e-100) == 4e-100 );
+        REQUIRE( my_abs(-3) == 3 );
+        REQUIRE( my_abs(-1e3) == 1e3 );
+        REQUIRE( my_abs(-4e-100) == 4e-100 );
     }
     SECTION( "Positive numbers stay positive" ) {
-        REQUIRE( abs(3) == 3 );
-        REQUIRE( abs(1e3) == 1e3 );
-        REQUIRE( abs(4e-100) == 4e-100 );
+        REQUIRE( my_abs(3) == 3 );
+        REQUIRE( my_abs(1e3) == 1e3 );
+        REQUIRE( my_abs(4e-100) == 4e-100 );
     }
 
     SECTION( "Infinites become +Inf" ) {
-        REQUIRE( abs(-std::numeric_limits<double>::infinity()) == std::numeric_limits<double>::infinity() );
-        REQUIRE( abs( std::numeric_limits<double>::infinity()) == std::numeric_limits<double>::infinity() );
+        REQUIRE( my_abs(-std::numeric_limits<double>::infinity()) == std::numeric_limits<double>::infinity() );
+        REQUIRE( my_abs( std::numeric_limits<double>::infinity()) == std::numeric_limits<double>::infinity() );
     }
 
     SECTION( "NaN stays NaN" ) {
         auto isnan = [](auto x){return x!=x;};
-        REQUIRE( isnan(abs(std::numeric_limits<double>::quiet_NaN())) );
+        REQUIRE( isnan(my_abs(std::numeric_limits<double>::quiet_NaN())) );
     }
 }
