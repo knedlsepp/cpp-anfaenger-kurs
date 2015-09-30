41ceb058aa304bbb93c81a7c5061de37531808ca
Josef Kemetmüller <josef.kemetmueller@zamg.ac.at>
2015-09-30 10:44:55 +0000

    Updated test cases

--- newtons_method/tests/test_all.cpp
+++ newtons_method/tests/test_all.cpp
@@ -5,21 +5,19 @@
 #include <cmath>
 
 
-TEST_CASE ( "newtons_method computes roots of double valued functions", "newtons_method"){
-
-    SECTION ( "sin has root at zero") {
-        REQUIRE( newtons_method(sin, cos, 0.1) == Approx(0);
+TEST_CASE ( "When using newtons_method", "newtons_method"){
+    SECTION ( "sin should have a root at zero") {
+        REQUIRE( newtons_method(sin, cos, 0.1) == Approx(0));
     }
-    SECTION ( "sin has root at pi") {
-
+    SECTION ( "sin should also have a root at pi") {
         REQUIRE( newtons_method(sin, cos, 3.12) == Approx(M_PI));
-    } 
-    SECTION ("newtons_method should be exact to 4 decimal places"){
-        REQUIRE( newtons_method(sin, cos, 3.12) == Approx(M_PI).epsilon(1e-4);
     }
-    SECTION ("newtons_method should work for lambdas"){
+    SECTION ("The method should be exact to at least 6 decimal places"){
+        REQUIRE( newtons_method(sin, cos, 3.12) == Approx(M_PI).epsilon(1e-6));
+    }
+    SECTION ("lambdas should be accepted as arguments"){
         REQUIRE( newtons_method([](double x){return x*x;},
                                 [](double x){return 2*x;},
-                                0.1) == Approx(0);
+                                0.1) == Approx(0));
     }
 }
