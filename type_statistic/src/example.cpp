d88a48801386997e4ed98d97269b907b4cddc87e
Josef Kemetmüller <josef.kemetmueller@aon.at>
2015-10-05 00:16:58 +0200

    Add necessary header.

--- type_statistic/src/example.cpp
+++ type_statistic/src/example.cpp
@@ -1,6 +1,9 @@
 #include <vector>
 #include <stdexcept>
 #include <algorithm>
+#include <unordered_map>
+#include <vector>
+
 auto type_statistic(std::vector<std::string>) -> std::unordered_map<std::string, unsigned int>{
     throw std::runtime_error("Not implemented");
 }
