5f3d471945c2cb83d2e48256304439b70dbb3cdc
Vortragender <vortragender@vdmmss-dev.zamg.ac.at>
2015-10-05 11:57:04 +0000

    Add empty file

---
+++ newtons_method/src/example.cpp
@@ -0,0 +1,6 @@
+#include <functional>
+double newtons_method(std::function<double(double)> f,
+                      std::function<double(double)> fprime,
+                      double initial_value){
+    return -123.;    
+}
