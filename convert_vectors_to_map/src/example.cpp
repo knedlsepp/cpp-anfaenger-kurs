59edf766a397734709d698ba96b2a20f5cf5f00e
Vortragender <vortragender@vdmmss-dev.zamg.ac.at>
2015-10-06 06:39:55 +0000

    Add vector to map conversion example.

---
+++ convert_vectors_to_map/src/example.cpp
@@ -0,0 +1,6 @@
+#include "example.hpp" 
+auto convert_vectors_to_map(const std::vector<double>& xs,
+                            const std::vector<double>& ys) -> std::map<double, double>{
+
+    throw "Not implemented";
+}
---
+++ convert_vectors_to_map/src/example.hpp
@@ -0,0 +1,4 @@
+#include <map>
+#include <vector>
+auto convert_vectors_to_map(const std::vector<double>& xs,
+                            const std::vector<double>& ys) -> std::map<double, double>;
