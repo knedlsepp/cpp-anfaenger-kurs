4293edaa1f9078fa10c22da95eb4a0a31615e20f
Vortragender <vortragender@vdmmss-dev.zamg.ac.at>
2015-10-06 06:57:52 +0000

    Add nearest smaller example.

---
+++ get_nearest_smaller/src/example.hpp
@@ -0,0 +1,4 @@
+#include <map>
+#include <tuple>
+
+auto get_nearest_smaller(std::map<double, double> f, double x) -> std::tuple<double,double>;
---
+++ get_nearest_smaller/tests/test_all.cpp
@@ -0,0 +1,18 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include <vector>
+#include "example.hpp"
+TEST_CASE( "When computing get_nearest_smaller", "[get_nearest_smaller]" ) {
+    SECTION( "the nearest smaller element should be returned." ) {
+        auto f = std::map<double, double>{{5.,3.}, {1.,2.}, {2.,3.},{1.5,0.}};
+        auto input = 1.6;
+        auto output = std::tuple<double, double>{1.5,0.};
+        REQUIRE( get_nearest_smaller(f, input) == output );
+    }
+    SECTION( "the nearest smaller element should be returned." ) {
+        auto f = std::map<double, double>{{5.,3.}, {1.,2.}, {2.,3.},{1.5,0.}};
+        auto input = 2.;
+        auto output = std::tuple<double, double>{2.,3.};
+        REQUIRE( get_nearest_smaller(f, input) == output );
+    }   
+}
