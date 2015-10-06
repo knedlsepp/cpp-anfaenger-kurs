73704c759e208f5e7b97651f22f9d661e11768b3
Vortragender <vortragender@vdmmss-dev.zamg.ac.at>
2015-10-06 13:16:01 +0000

    Added tests.

--- LinearInterpolator/tests/test_all.cpp
+++ LinearInterpolator/tests/test_all.cpp
@@ -1,3 +1,12 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include <vector>
+#include "example.cpp"
+TEST_CASE ( "When doing linear interpolation", "[LinearInterpolator]" ){
+    SECTION( "The interpolator object should be created and evaluatable"){
+        auto lin = LinearInterpolator{{1.,2.,3.}, {4.,10.,8.}};
+        CHECK (lin.interpolate(1.5) == Approx(7.) );
+        CHECK (lin.interpolate(2.5) == Approx(9.) );
+    }
+}
+
