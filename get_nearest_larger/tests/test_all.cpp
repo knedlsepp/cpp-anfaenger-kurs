18338382da2c1dba7821b1c226635318094c495f
Vortragender <vortragender@vdmmss-dev.zamg.ac.at>
2015-10-06 11:06:21 +0000

    Correct from smaller to larger

--- get_nearest_larger/tests/test_all.cpp
+++ get_nearest_larger/tests/test_all.cpp
@@ -7,12 +7,12 @@ TEST_CASE( "When computing get_nearest_larger", "[get_nearest_larger]" ) {
         auto f = std::map<double, double>{{5.,3.}, {1.,2.}, {2.,3.},{1.5,0.}};
         auto input = 1.6;
         auto output = std::tuple<double, double>{2.,3.};
-        REQUIRE( get_nearest_smaller(f, input) == output );
+        REQUIRE( get_nearest_larger(f, input) == output );
     }
     SECTION( "the nearest larger element should be returned." ) {
         auto f = std::map<double, double>{{5.,3.}, {1.,2.}, {2.,3.},{1.5,0.}};
         auto input = 2.1;
         auto output = std::tuple<double, double>{5.,3.};
-        REQUIRE( get_nearest_smaller(f, input) == output );
+        REQUIRE( get_nearest_larger(f, input) == output );
     }   
 }
